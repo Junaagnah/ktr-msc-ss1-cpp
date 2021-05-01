fa6db65bcdf47941a2e254130191f4924d3c7102
GEHRINGER Nicolas <nicolas.gehringer@outlook.fr>
2021-05-01 18:52:59 +0200

    Initial commit

---
+++ ktr-msc-ss1/Warrior.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Character.h"
+#include <string>
+
+namespace ktrmscss1
+{
+	class Warrior : public Character
+	{
+		public:
+			explicit Warrior(std::string name, std::string RPGClass = "Warrior");
+			void attack(std::string weapon) override;
+			void moveRight() override;
+			void moveLeft() override;
+			void moveForward() override;
+			void moveBack() override;
+	};
+}
+
